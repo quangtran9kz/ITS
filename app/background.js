95a42695be2ae452b5febb94b4482d8b1d082ce3
QuangTran <quangtran10kz@gmail.com>
2018-11-14 16:52:53 +0700

    Fix ham format URL

--- app/background.js
+++ app/background.js
@@ -1,46 +1,82 @@
+// Hepler functions
+function convertUrlToUri(urlString){  
+    try{
+        var paraArray = urlString.split("?")[1].split("&");
+        var uri = {};
+        for(let para = 0; para < paraArray.length; para++){
+            let key = paraArray[para].split("=")[0];
+            let value = paraArray[para].split("=")[1];
+            uri[key] = value;
+        }
+        return uri;
+    } catch(error){
+        return ;
+    }
+    
+}
+
 (function() {
     const tabStorage = {};
     const networkFilters = {
         urls: [
-            "http://20.203.139.2:8080/AssignmentLoginScreen/*"
+            "<all_urls>"
         ]
     };
-
+    // Capture HTTP request
     chrome.webRequest.onBeforeRequest.addListener((details) => {
-        const { tabId, requestId, url, timeStamp , method} = details;
+        const { tabId, requestId, url, timeStamp, method, type, frameId, parentFrameId } = details;
         if (!tabStorage.hasOwnProperty(tabId)) {
             return;
         }
-
+        // Capture HTTP request, action method = POST
+        if(details.method == "POST"){            
+            var data = details.requestBody.formData;
+            console.log("Form Data:");
+            console.log(data);
+            /*
+            if(data) {
+                Object.keys(data).forEach(key => {
+                    data[key].forEach(value => {
+                        console.log(value);
+                    });
+                });
+            }
+            */
+        }
+        // Capture HTTP request, action method = GET
         tabStorage[tabId].requests[requestId] = {
             requestId: requestId,
+            method: method,
+            type: type,
+            frameId: frameId,
             url: url,
             startTime: timeStamp,
+            parentFrameId: parentFrameId,
             status: 'pending'
         };
+        // split URL
+        var urlString = tabStorage[tabId].requests[requestId].url;
+        console.log("URL:");
+        //console.log(typeof (urlString));
+        console.log(urlString);        
+        var uri = {};
+        uri = convertUrlToUri(urlString);        
+        console.log("Request infomation:");
         console.log(tabStorage[tabId].requests[requestId]);
-
-        if(details.method == "POST") {
-            console.log(details.requestBody.formData);
-           
-            let formData = details.requestBody.formData;
-            if(formData) {
-                Object.keys(formData).forEach(key => {
-                  formData[key].forEach(value => {
-                    console.log(value);
-                  });
-                });
-              }
-        }
-
-    }, networkFilters, ["requestBody"]);
+        //console.log(paraArray);
+        console.log("Uri:");
+        console.log(uri);
+        
+    }, networkFilters,["requestBody"]);
 
     chrome.webRequest.onCompleted.addListener((details) => {
-        const { tabId, requestId, timeStamp } = details;
+        const { tabId, requestId, timeStamp, responseHeaders } = details;
         if (!tabStorage.hasOwnProperty(tabId) || !tabStorage[tabId].requests.hasOwnProperty(requestId)) {
             return;
         }
-
+        let responseHeader = {...responseHeaders};
+        console.log("Response:");
+        console.log(responseHeader);
         const request = tabStorage[tabId].requests[requestId];
 
         Object.assign(request, {
@@ -49,8 +85,9 @@
             status: 'complete'
         });
         console.log(tabStorage[tabId].requests[details.requestId]);
-    }, networkFilters);
+    }, networkFilters, ["responseHeaders"]);
 
+    // When errors
     chrome.webRequest.onErrorOccurred.addListener((details)=> {
         const { tabId, requestId, timeStamp } = details;
         if (!tabStorage.hasOwnProperty(tabId) || !tabStorage[tabId].requests.hasOwnProperty(requestId)) {
@@ -95,3 +132,4 @@
         }
     });
 }());
+
