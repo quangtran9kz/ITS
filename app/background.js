2b3744a25b07105a500412893c7e1943ea366ac9
QuangTran <quangtran10kz@gmail.com>
2018-11-14 16:38:33 +0700

    Thêm hàm format url

--- app/background.js
+++ app/background.js
@@ -2,23 +2,16 @@
     const tabStorage = {};
     const networkFilters = {
         urls: [
-            "<all_urls>"
+            "http://20.203.139.2:8080/AssignmentLoginScreen/*"
         ]
     };
 
     chrome.webRequest.onBeforeRequest.addListener((details) => {
-        const { tabId, requestId, url, timeStamp } = details;
+        const { tabId, requestId, url, timeStamp , method} = details;
         if (!tabStorage.hasOwnProperty(tabId)) {
             return;
         }
-        if(details.method=="POST"){
-           try {
-            var data=details.requestBody.formData !==undefined ?details.requestBody.formData: "";
-            console.log(data);
-           } catch (error) {
-               console.log(error);
-           }
-        }
+
         tabStorage[tabId].requests[requestId] = {
             requestId: requestId,
             url: url,
@@ -26,15 +19,28 @@
             status: 'pending'
         };
         console.log(tabStorage[tabId].requests[requestId]);
-    }, networkFilters,["requestBody"]);
+
+        if(details.method == "POST") {
+            console.log(details.requestBody.formData);
+           
+            let formData = details.requestBody.formData;
+            if(formData) {
+                Object.keys(formData).forEach(key => {
+                  formData[key].forEach(value => {
+                    console.log(value);
+                  });
+                });
+              }
+        }
+
+    }, networkFilters, ["requestBody"]);
 
     chrome.webRequest.onCompleted.addListener((details) => {
-        const { tabId, requestId, timeStamp ,responseHeaders} = details;
+        const { tabId, requestId, timeStamp } = details;
         if (!tabStorage.hasOwnProperty(tabId) || !tabStorage[tabId].requests.hasOwnProperty(requestId)) {
             return;
         }
-        let responseHeader={...responseHeaders};
-        console.log(responseHeader);
+
         const request = tabStorage[tabId].requests[requestId];
 
         Object.assign(request, {
@@ -43,7 +49,7 @@
             status: 'complete'
         });
         console.log(tabStorage[tabId].requests[details.requestId]);
-    }, networkFilters,["responseHeaders"]);
+    }, networkFilters);
 
     chrome.webRequest.onErrorOccurred.addListener((details)=> {
         const { tabId, requestId, timeStamp } = details;
