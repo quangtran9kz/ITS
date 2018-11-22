b7ae2ad02afc22afd8cac9761b0053d91820a0b6
QuangTran <quangtran10kz@gmail.com>
2018-11-22 19:50:59 +0700

    Xóa code thừa

--- app/content.js
+++ app/content.js
@@ -1,5 +1,4 @@
 var doc = document.body.innerHTML
-console.log(doc);
 //var result = JSON.stringify(x);
 chrome.runtime.sendMessage({dom: doc}, function(response) {
     console.log("Response: ", response);
